5c6dc2025c69ffedb5153ec1715c20ec50f8fcf2
Partho Debnath <parthodebnath28@gmail.com>
2023-05-20 03:14:01 +0600

    interface basic

---
+++ Dart Oop/9 Interface/1 interface.dart
@@ -0,0 +1,31 @@
+class BaseClass {
+  int? a;
+  BaseClass(this.a) {
+    a = 1111;
+    print('BaseClass Constructor');
+  }
+
+  void fnc() {
+    print('From BaseClass fnc function');
+  }
+}
+
+class DerivedClass implements BaseClass {
+  int? a;
+  String? name;
+  DerivedClass() {
+    a = 333;
+    name = 'Partho';
+    print('DerivedClass Constructor');
+  }
+
+  @override
+  void fnc() {
+    print('From BaseClass fnc function. Name: $name a: ${this.a}');
+  }
+}
+
+void main() {
+  DerivedClass derivedClass = DerivedClass();
+  derivedClass.fnc();
+}
---
+++ Dart Oop/9 Interface/2 interface.dart
@@ -0,0 +1,48 @@
+abstract class Vehicle {
+  String carName;
+  Vehicle({required this.carName});
+
+  /// Abstract method
+  void start() {
+    print('Car start. car Name: $carName');
+  }
+
+  /// Abstract method
+  void stop();
+
+  void fnc() {
+    print('From Vehicle class fnc function');
+  }
+}
+
+class Car implements Vehicle {
+  late String carName;
+  int numberOfDroor;
+  Car({required String myCarName, required this.numberOfDroor}) {
+    carName = myCarName;
+  }
+
+  /// Implementation of start()
+  @override
+  void start() {
+    print('Car started. Brand Name: $carName. Doors : $numberOfDroor');
+  }
+
+  /// Implementation of stop()
+  @override
+  void stop() {
+    print('Car stopped');
+  }
+
+  @override
+  void fnc() {
+    print('From Car class fnc function. Running.. Speed: 500 KMH');
+  }
+}
+
+void main() {
+  Car car = Car(myCarName: 'BMW', numberOfDroor: 4);
+  car.start();
+  car.fnc();
+  car.stop();
+}
