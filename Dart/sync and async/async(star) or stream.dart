de9a93365c370691f5f484dcf9dd51d1f0ebe575
Partho Debnath <parthodebnath28@gmail.com>
2023-07-24 18:37:26 +0600

    update async* or Stream function

--- Dart/sync and async/async(star) or stream.dart
+++ Dart/sync and async/async(star) or stream.dart
@@ -1,16 +1,20 @@
 void main() async {
   print('main Start');
 
-  await for (var data in getDataTwo()) {
+  await for (var data in getDataOne(10)) {
     print(data);
   }
+
+  print('main End');
 }
 
 Stream<String> getDataOne(int range) async* {
+  print('---Start getDataOne(10)---');
   for (int i = 1; i < range; i++) {
     yield 'Index: $i';
     await Future<void>.delayed(const Duration(seconds: 1));
   }
+  print('---End getDataOne(10)---');
 }
 
 Stream<String> getDataTwo() async* {
